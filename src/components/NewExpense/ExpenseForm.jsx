7a389539328dc71e6f0ff28f656b9febba11c29c
jplaulau14 <code.patricklaurel@gmail.com>
2022-09-25 23:19:03 +0800

    change amount from string to int

--- src/components/NewExpense/ExpenseForm.jsx
+++ src/components/NewExpense/ExpenseForm.jsx
@@ -22,7 +22,7 @@ const ExpenseForm = (props) => {
     event.preventDefault();
     const newExpense = {
       title: enteredTitle,
-      amount: enteredAmount,
+      amount: +enteredAmount,
       date: new Date(enteredDate),
     };
 
