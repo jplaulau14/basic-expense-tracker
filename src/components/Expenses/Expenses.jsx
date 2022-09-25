39bff9af183c34a5c0951f5543a78f5dbef8ea19
jplaulau14 <code.patricklaurel@gmail.com>
2022-09-25 14:54:09 +0800

    added initial filter

--- src/components/Expenses/Expenses.jsx
+++ src/components/Expenses/Expenses.jsx
@@ -1,11 +1,17 @@
 import React, { useState } from "react";
 
 import ExpenseItem from "./ExpenseItem";
+import ExpensesFilter from "./ExpensesFilter";
 import "./Expenses.css";
 
 const Expenses = (props) => {
+  const [filteredYear, setFilteredYear] = useState("2022");
+  const filterChangeHandler = selectedYear => {
+    setFilteredYear(selectedYear);
+  }
   return (
     <div className="expenses">
+      <ExpensesFilter selected={filteredYear} onFilteredYear={filterChangeHandler}/>
       {props.items.map((expense) => (
         <ExpenseItem
           date={expense.date}
